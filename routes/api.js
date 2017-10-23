a1c698677edc464e9ec96f147e52cd66799e5fe2
Xinran Wang <xw1992@gmail.com>
2017-10-23 01:39:46 -0400

    Add apidocs

--- routes/api.js
+++ routes/api.js
@@ -5,16 +5,46 @@ const fs = Promise.promisifyAll(require('fs'))
 const DAO = require('./../data/DAO')
 const { Site } = require('./../models')
 
-router.get('/stations/:stationId/tanks', async (req, res, next) => {
+/**
+ * @api {get} /api/stations Get stations
+ * @apiName GetStations
+ * @apiGroup Stations
+ *
+ * @apiExample Example usage:
+ * http http://localhost/api/stations
+ * 
+ * @apiSuccess {Station[]} response List of Stations (`response` is only a placeholder)
+ * @apiSuccess {Number} response.id Station's unique Id
+ * @apiSuccess {String} response.name The name of the Station
+ * @apiSuccess {Boolean} response.compliant The compliance state of the Station
+ * @apiSuccess {Boolean} response.complianceUpdate Whether the Station's compliance 
+ *                                                 state has been manually updated
+ */
+router.get('/stations', async (req, res, next) => {
   try {
-    const tanks = await DAO.getTanks(req.params.stationId)
-    res.json(tanks)
+    const sites = await DAO.getSites()
+    res.json(sites)
   } catch (err) {
-    err.message = `There was a problem fetching tanks for station ${req.params.stationId}`
     next(err)
   }
 })
 
+/**
+ * @api {get} /api/stations/:stationId Get Station
+ * @apiName GetStation
+ * @apiGroup Station
+ *
+ * @apiParam {Number} stationId Station's unique Id
+ *
+ * @apiExample Example usage:
+ * http http://localhost/api/stations/123
+ * 
+ * @apiSuccess {Number} id Station's unique Id
+ * @apiSuccess {String} name The name of the Station
+ * @apiSuccess {Boolean} compliant The compliance state of the Station
+ * @apiSuccess {Boolean} complianceUpdate Whether the Station's compliance 
+ *                                        state has been manually updated
+ */
 router.get('/stations/:stationId', async (req, res, next) => {
   try {
     const site = await DAO.getSite(req.params.stationId)
@@ -29,6 +59,18 @@ router.get('/stations/:stationId', async (req, res, next) => {
   }
 })
 
+/**
+ * @api {post} /station/:stationId Update Station
+ * @apiName UpdateStation
+ * @apiGroup Station
+ *
+ * @apiDescription Currently only supportings updating Station's compliance by posting `{ compliance: true/false }` as form urlencoded paramters
+ *
+ * @apiParam {String} stationId Station's unique Id
+ * 
+ * @apiExample Example usage:
+ * http --form POST http://localhost/api/stations/123 compliance=true
+ */
 router.post('/stations/:stationId', async (req, res, next) => {
   try {
     const updatedSite = await DAO.updateSite(req.params.stationId, req.body)
@@ -38,11 +80,27 @@ router.post('/stations/:stationId', async (req, res, next) => {
   }
 })
 
-router.get('/stations', async (req, res, next) => {
+/**
+ * @api {get} /stations/:stationId/tanks Get Tanks for Station
+ * @apiName GetTanks
+ * @apiGroup Tanks
+ *
+ * @apiParam {Number} stationId Station's unique id
+ *
+ * @apiExample Example usage:
+ * http http://localhost/api/stations/123/tanks
+ * 
+ * @apiSuccess {Tank[]} response List of Tanks (`response` is only a placeholder)
+ * @apiSuccess {String} response.name Name of the Tank
+ * @apiSuccess {Boolean} response.leaking Whether the Tank has a leak
+ * @apiSuccess {Date} response.leakStatusDate ???
+ */
+router.get('/stations/:stationId/tanks', async (req, res, next) => {
   try {
-    const sites = await DAO.getSites()
-    res.json(sites)
+    const tanks = await DAO.getTanks(req.params.stationId)
+    res.json(tanks)
   } catch (err) {
+    err.message = `There was a problem fetching tanks for station ${req.params.stationId}`
     next(err)
   }
 })
